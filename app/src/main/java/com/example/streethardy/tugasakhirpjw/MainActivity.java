d49558ab89c78539235810cc625a6379ddea4c71
Hardian Prakasa <hardian.prakasa@mail.ugm.ac.id>
2015-06-23 12:38:31 +0700

    Mendefenisikan element UI dan menambah method untuk insert data

--- app/src/main/java/com/example/streethardy/tugasakhirpjw/MainActivity.java
+++ app/src/main/java/com/example/streethardy/tugasakhirpjw/MainActivity.java
@@ -1,18 +1,121 @@
 package com.example.streethardy.tugasakhirpjw;
 
+import android.os.AsyncTask;
 import android.support.v7.app.ActionBarActivity;
 import android.os.Bundle;
 import android.view.Menu;
 import android.view.MenuItem;
+import android.view.View;
+import android.widget.EditText;
+import android.widget.ListAdapter;
+import android.widget.ListView;
+import android.widget.SimpleAdapter;
+import android.widget.TextView;
+import android.widget.Toast;
+
+import org.apache.http.HttpEntity;
+import org.apache.http.HttpResponse;
+import org.apache.http.NameValuePair;
+import org.apache.http.client.ClientProtocolException;
+import org.apache.http.client.HttpClient;
+import org.apache.http.client.entity.UrlEncodedFormEntity;
+import org.apache.http.client.methods.HttpPost;
+import org.apache.http.impl.client.DefaultHttpClient;
+import org.apache.http.message.BasicNameValuePair;
+import org.apache.http.params.BasicHttpParams;
+import org.json.JSONArray;
+import org.json.JSONException;
+import org.json.JSONObject;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStream;
+import java.io.InputStreamReader;
+import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.List;
 
 
 public class MainActivity extends ActionBarActivity {
+
+    private EditText editTextName;
+    private EditText editTextAdd;
+
+
+    String myJSON;
+
+    private static final String TAG_RESULTS="result";
+    private static final String TAG_ID = "id";
+    private static final String TAG_NAME = "name";
+    private static final String TAG_ADD ="address";
+
+    JSONArray peoples = null;
+
+    ArrayList<HashMap<String, String>> personList;
+
+    ListView list;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        editTextName = (EditText) findViewById(R.id.editTextName);
+        editTextAdd = (EditText) findViewById(R.id.editTextAddress);
+
+        list = (ListView) findViewById(R.id.listView);
+        personList = new ArrayList<HashMap<String,String>>();
+        getData();
+
     }
+
+    public void insert(View view){
+
+        String name = editTextName.getText().toString();
+        String add = editTextAdd.getText().toString();
+
+        insertToDatabase(name, add);
+        personList.clear();
+        list.setAdapter(null);
+
+        getData();
+
+    }
+
+    protected void insertToDatabase(String name, String add){
+        class SendPostReqAsyncTask extends AsyncTask<String, Void, String> {
+            @Override
+            protected String doInBackground(String... params) {
+
+                String paramUsername = params[0];
+                String paramAddress = params[1];
+
+
+                String name = editTextName.getText().toString();
+                String add = editTextAdd.getText().toString();
+
+                List<NameValuePair> nameValuePairs = new ArrayList<NameValuePair>();
+                nameValuePairs.add(new BasicNameValuePair("name", name));
+                nameValuePairs.add(new BasicNameValuePair("address", add));
+
+                try {
+                    HttpClient httpClient = new DefaultHttpClient();
+                    HttpPost httpPost = new HttpPost("http://tespjw.esy.es/insert-db.php");
+                    httpPost.setEntity(new UrlEncodedFormEntity(nameValuePairs));
+
+                    HttpResponse response = httpClient.execute(httpPost);
+
+                    HttpEntity entity = response.getEntity();
+
+
+                } catch (ClientProtocolException e) {
+
+                } catch (IOException e) {
+
+                }
+                return "success";
+            }
+
 
     @Override
     public boolean onCreateOptionsMenu(Menu menu) {
